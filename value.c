61c876d68de7d57d2d4dc5cf44596d9033abfb54
chaker <chaker@localhost.localdomain>
2024-08-06 01:17:18 +0100

    Implemented Strings

--- value.c
+++ value.c
@@ -1,31 +1,36 @@
-#include <stdio.h>
-
-#include "memory.h"
 #include "value.h"
+#include "memory.h"
+#include "object.h"
+#include <stdio.h>
+#include <string.h>
 
-void initValueArray(ValueArray *array) {
-	array->values = NULL;
-	array->capacity = 0;
-	array->count = 0;
+void initValueArray(ValueArray* array)
+{
+    array->values = NULL;
+    array->capacity = 0;
+    array->count = 0;
 }
 
-void writeValueArray(ValueArray *array, Value value) {
-	if(array->capacity < array->count + 1) {
-		int oldCapacity = array->capacity;
-		array->capacity = GROW_CAPACITY(oldCapacity);
-		array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
-	}
+void writeValueArray(ValueArray* array, Value value)
+{
+    if (array->capacity < array->count + 1) {
+        int oldCapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldCapacity);
+        array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
+    }
 
-	array->values[array->count] = value;
-	array->count++;
+    array->values[array->count] = value;
+    array->count++;
 }
 
-void freeValueArray(ValueArray *array){
-	FREE_ARRAY(Value, array->values, array->capacity);
-	initValueArray(array);
+void freeValueArray(ValueArray* array)
+{
+    FREE_ARRAY(Value, array->values, array->capacity);
+    initValueArray(array);
 }
 
-void printValue(Value value){
+void printValue(Value value)
+{
     switch (value.type) {
     case VAL_BOOL:
         printf(AS_BOOL(value) ? "true" : "false");
@@ -35,6 +40,9 @@ void printValue(Value value){
         break;
     case VAL_NUMBER:
         printf("%g", AS_NUMBER(value));
+        break;
+    case VAL_OBJ:
+        printObject(value);
         break;
     }
 }
@@ -50,6 +58,11 @@ bool valuesEqual(Value a, Value b)
         return true;
     case VAL_NUMBER:
         return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ: {
+        ObjString* aString = AS_STRING(a);
+        ObjString* bString = AS_STRING(b);
+        return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+    }
     default:
         return false; // Unreachable.
     }
